0423b83ad6cfec5a5f2ea957c7afa67f24db1648
Clayton Craft <clayton@craftyguy.net>
2021-05-06 13:19:15 -0700

    Initial commit of application

---
+++ README.md
@@ -0,0 +1,56 @@
+An app for sharing GNSS location data, with support multiple clients and
+loading/saving AGPS data.
+
+This is meant to replace things like `gpsd`, and `gps-share`, and work together
+with `geoclue`* or other clients that support fetching NMEA location data over
+sockets.
+
+*To work with `geoclue`, [these patches are required.](https://gitlab.freedesktop.org/geoclue/geoclue/-/merge_requests/79/diffs) A future version of
+`geoclue` should work out of the box after pointing it to the socket created by
+this app.
+
+# Configuration
+
+`gnss_share.conf` can be used to change the listening socket, group owner for
+socket, and other options. The application looks for this file in either the
+current working directory, or in `/etc/gnss_share.conf`.
+
+See this file for descriptions of supported options.
+
+# Installation
+
+### Dependencies:
+
+- [Trio](https://github.com/python-trio/trio)
+- [pynmea2](https://github.com/Knio/pynmea2)
+
+This project uses meson to "build" and install things:
+
+```
+$ meson _build
+$ meson install -C _build
+```
+
+To run locally from the source repo:
+
+```
+$ python3 -m gnss_share
+```
+
+# Development
+
+### New GNSS device support
+
+Support for additional gnss devices can be added by implementing a new 'driver', see `stm_agps.py` for an example.
+At a minimum, `gnss_share` expects that the following methods are implemented in the driver:
+
+- context manager support (`__aenter__`, `__aexit__`)
+- `load(directory: str) -> None`
+- `store(directory: str) -> None`
+- `open() -> None`
+- `close() -> None`
+- `readline() -> bytes`
+- `reset() -> None`
+- `settime() -> None`
+
+This application uses the Python Trio library for async coroutines, so blocking tasks in the driver must be made async.
---
+++ gnss_share/stm_agps.py
@@ -0,0 +1,148 @@
+# Copyright(c) 2021 by Angus Ainslee
+# Copyright(c) 2021 by Purism SPC
+# Copyright(c) 2021 by craftyguy "Clayton Craft" <clayton@craftyguy.net>
+# Distributed under GPLv3+ (see COPYING) WITHOUT ANY WARRANTY.
+import logging
+import os
+import pynmea2
+import trio
+from datetime import datetime
+
+from .logger import LoggedException
+
+
+class STM_AGPS:
+
+    def __init__(self, serial_port):
+        self.__log = logging.getLogger(__name__)
+        if not os.path.exists(serial_port):
+            raise LoggedException("Serial port does not exist: "
+                                  f"{serial_port}")
+        self._ser_port = serial_port
+        self._location = b""
+
+    async def __aenter__(self):
+        await self.open()
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.close()
+
+    async def close(self):
+        await self._ser.aclose()
+
+    async def open(self):
+        try:
+            self._ser = await trio.open_file(self._ser_port,
+                                             "w+b", buffering=0)
+        except Exception as e:
+            raise LoggedException(e)
+
+    async def readline(self):
+        return await self._ser.readline()
+
+    async def _serial_write_cmd(self, cmd, expect=None):
+        # number of times to poll serial output for ACK after sending command
+        polling_loops = 50
+
+        self.__log.info(f"cmd: {cmd}")
+        await self._ser.write(str(cmd).encode("ascii"))
+        await self._ser.write(b'\r\n')
+        if expect:
+            for i in range(polling_loops):
+                line = await self._ser.readline()
+                line = line[:-1]
+                self.__log.info(f"read: {line}")
+                if expect.encode("ascii") in line:
+                    self.__log.info(f"found: {expect}: {line}")
+                    return True, line
+
+            self.__log.info(f"not found: {expect}: {line}")
+            return False, line
+
+        # wait for cmd completion
+        for i in range(polling_loops):
+            line = await self._ser.readline()
+            line = line[:-1]
+            self.__log.info(f"read: {line}")
+            if str(cmd).encode("ascii") in line:
+                return True, line
+
+        return False, line
+
+    async def _store_to_file(self, cmd, ack, file):
+        msg = pynmea2.GGA('P', cmd, ())
+        result, line = await self._serial_write_cmd(msg, ack)
+        if not result:
+            raise LoggedException("Unable to get data from device")
+        async with await trio.open_file(file, 'wb') as f:
+            while True:
+                if cmd.encode() in line:
+                    return
+                if line.startswith(ack.encode()):
+                    self.__log.info(line)
+                    await f.write(line + b'\n')
+                line = await self._ser.readline()
+                line = line[:-1]
+
+    async def reset(self):
+        msg = pynmea2.GGA('P', 'STMGPSRESET', ())
+        await self._serial_write_cmd(msg)
+        await trio.sleep(1)
+
+    async def store(self, dir):
+        almanac_path = os.path.join(dir, 'almanac.txt')
+        ephemeris_path = os.path.join(dir, 'ephemeris.txt')
+
+        # reset device in case it is stuck
+        await self.reset()
+
+        await self._store_almanac(almanac_path)
+        await self._store_ephemeris(ephemeris_path)
+
+    async def load(self, dir):
+        almanac_path = os.path.join(dir, 'almanac.txt')
+        ephemeris_path = os.path.join(dir, 'ephemeris.txt')
+
+        for file in [almanac_path, ephemeris_path]:
+            if not os.path.exists(almanac_path):
+                self.__log.warn(f"AGPS file not found: {file}")
+                self.__log.warn("*NOT* loading AGPS data")
+                return
+
+        # reset device in case it is stuck, and set time
+        await self.reset()
+        await self.set_time()
+
+        await self._load_almanac(almanac_path)
+        await self._load_ephemeris(ephemeris_path)
+
+    async def _store_ephemeris(self, file='ephemeris.txt'):
+        await self._store_to_file('STMDUMPEPHEMS', '$PSTMEPHEM', file)
+
+    async def _store_almanac(self, file='almanac.txt'):
+        await self._store_to_file('STMDUMPALMANAC', '$PSTMALMANAC', file)
+
+    async def _load_ephemeris(self, file='ephemeris.txt'):
+        await self._store_to_file('STMDUMPEPHEMS', '$PSTMEPHEM', file)
+
+    async def _load_almanac(self, file='almanac.txt'):
+        await self._store_to_file('STMDUMPALMANAC', '$PSTMALMANAC', file)
+
+    async def set_time(self):
+        await self.reset()
+        now = datetime.utcnow()
+
+        # INITTIME expects values to be 2 or 4 digits long
+        msg = pynmea2.GGA('P', 'STMINITTIME', (
+            now.strftime('%d'),
+            now.strftime('%m'),
+            now.strftime('%Y'),
+            now.strftime('%H'),
+            now.strftime('%M'),
+            now.strftime('%S'),
+            ))
+        ret, line = await self._serial_write_cmd(msg, "STMINITTIMEOK")
+        if not ret:
+            raise LoggedException(f"ERROR: {line}")
+        self.__log.info(line)
