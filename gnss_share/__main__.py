fa85a36c5011e5c429faed648f70db51a4c61f3e
Clayton Craft <clayton@craftyguy.net>
2021-11-21 15:01:53 -0800

    remove old python-based app

--- gnss_share/__main__.py
+++
@@ -1,7 +0,0 @@
-# Copyright(c) 2021 by craftyguy "Clayton Craft" <clayton@craftyguy.net>
-# Distributed under GPLv3+ (see COPYING) WITHOUT ANY WARRANTY.
-import sys
-
-if __name__ == '__main__':
-    from gnss_share import main
-    sys.exit(main.main("dev"))
